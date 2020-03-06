345b4a1025bd8f7dfec5409c6a2945577fb05ab6
MadsMogensen <mega-mads@hotmail.com>
2020-03-06 14:29:55 +0100

    Working on server Connection

--- app/src/main/java/com/example/positioningapp/MainActivity.java
+++ app/src/main/java/com/example/positioningapp/MainActivity.java
@@ -3,15 +3,21 @@ package com.example.positioningapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public TextView label;
+    Thread serverThread = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-        //test
+        label = findViewById(R.id.outputLabel);
+        ServerConnector serverConnection = new ServerConnector();
+        serverThread = new Thread(serverConnection);
+        serverThread.start();
+        label.setText(serverConnection.serverStatus);
     }
 }

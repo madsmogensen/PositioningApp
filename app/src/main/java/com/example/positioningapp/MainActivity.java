5c18bef0ffa074c456b9f4880b0feba7c2afcb75
MadsMogensen <mega-mads@hotmail.com>
2020-03-09 14:14:00 +0100

    Now connecting to the server on startup Button to connect/disconnect to the server Missing: What happens if the server is unavailable?

--- app/src/main/java/com/example/positioningapp/MainActivity.java
+++ app/src/main/java/com/example/positioningapp/MainActivity.java
@@ -3,21 +3,66 @@ package com.example.positioningapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
     public TextView label;
-    Thread serverThread = null;
+    public Button button;
+    ServerConnector serverConnection = null;
+    Boolean running = true;
+    Boolean connectToServer = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initialize();
+
+        while(running){
+            update();
+        }
+    }
+
+    private void update(){
+        label.setText(serverConnection.connectionStatus.toString());
+        //checkServer();
+        running = false;
+    }
+
+    public void buttonConnectToServer(View view){
+        System.out.println("Button clicked");
+        connectToServer = !connectToServer;
+    }
+
+    private void checkServer(){
+        if(serverConnection == null && connectToServer){
+            try{
+                serverConnection = new ServerConnector();
+            }catch(Exception e){
+                System.out.println(e);
+            }
+        }else if(serverConnection.connectionStatus && !connectToServer){
+            try{
+                serverConnection.stopConnection();
+            }catch(Exception e){
+                System.out.println("MainActivity stop connection: " + e);
+            }
+        }
+    }
+
+    private void initialize(){
         label = findViewById(R.id.outputLabel);
-        ServerConnector serverConnection = new ServerConnector();
-        serverThread = new Thread(serverConnection);
-        serverThread.start();
-        label.setText(serverConnection.serverStatus);
+        button = findViewById(R.id.ServerButton);
+        try{
+            serverConnection = new ServerConnector();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
